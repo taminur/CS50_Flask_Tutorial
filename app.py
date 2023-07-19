4c9368ef86a31077a1af4797691d265476fad297
taminur <taminur@live.com>
2023-07-19 20:23:44 +0600

    Adding form to the index.html and create a diffent route for greet.html

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__) # __name__ refers to the name of the current file; this line tells "Turns this file into a flask application"
+app = Flask(__name__) 
 
 @app.route("/")
 def index():
-    # if "name" in request.args:
-    #     name = request.args["name"]
-    # else:
-    #     name = "world"
-    name = request.args.get("name", "world") # instead of above four lines, we can use only this line, which works same
-    return render_template("index.html", placeholder=name) # browser address eg. local host/?name="Tamim"
+    return render_template("index.html")
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
