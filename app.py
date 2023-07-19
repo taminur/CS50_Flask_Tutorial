7ca823743f8b41fdfd0a54927a8d82ab6beccf3f
taminur <taminur@live.com>
2023-07-19 17:38:13 +0600

    v1.1:single line placeholder

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__) # __name__ refers to the name of the current file; this li
 
 @app.route("/")
 def index():
-    if "name" in request.args:
-        name = request.args["name"]
-    else:
-        name = "world"
+    # if "name" in request.args:
+    #     name = request.args["name"]
+    # else:
+    #     name = "world"
+    name = request.args.get("name", "world") # instead of above four lines, we can use only this line, which works same
     return render_template("index.html", placeholder=name) # browser address eg. local host/?name="Tamim"
